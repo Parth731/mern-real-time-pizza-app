8660fba11eacb673b7bda0abd21cc58c9a42cb64
divyadangroshiya <divya.dangroshiya21@gmail.com>
2023-12-31 14:27:56 +0530

    make cart and auth page

--- server.js
+++ server.js
@@ -10,19 +10,34 @@ const PORT = process.env.PORT || 3300
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
 
-// Assets
-app.use(express.static('public'))
-
-// routes
-app.use("/", (req, res) => {
-    res.render("home")
-})
-
-
 // set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+
+
+// Assets
+app.use(express.static('public'))
+
+// routes
+app.use("/home", (req, res) => {
+    res.render("home")
+})
+
+app.use("/cart", (req, res) => {
+    res.render("customers/cart")
+})
+
+app.use("/login", (req, res) => {
+    res.render("auth/login")
+})
+
+app.use("/register", (req, res) => {
+    res.render("auth/register")
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`listing on port ${PORT}`);
