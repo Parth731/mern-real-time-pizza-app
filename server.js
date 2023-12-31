9d621f63a16ea8b7178ee0eb49354a4f79ea7dbf
divyadangroshiya <divya.dangroshiya21@gmail.com>
2023-12-31 13:14:48 +0530

    make a home page

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-// const express = require('express')
-import express from 'express';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-import expressLayout from 'express-ejs-layouts';
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
 
 
 // constant
 const app = express()
 const PORT = process.env.PORT || 3300
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// Assets
+app.use(express.static('public'))
 
 // routes
 app.use("/", (req, res) => {
@@ -19,7 +21,7 @@ app.use("/", (req, res) => {
 
 // set Template engine
 app.use(expressLayout)
-app.set('views', join(__dirname, '/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
